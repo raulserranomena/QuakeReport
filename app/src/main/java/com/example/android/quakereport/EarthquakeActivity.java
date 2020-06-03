850f6afd048d02e3cf2a81921c9d65f9843fa382
Raul Serrano Mena <raulserranomena@gmail.com>
2020-06-03 14:47:56 -0500

    EarthquakeApp, Construct Desired URL from User Minimum Magnitude Preference

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -25,6 +26,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.content.Loader;
 
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -51,7 +53,7 @@ public class EarthquakeActivity extends AppCompatActivity implements androidx.lo
     /**
      * URL for earthquake data from the USGS dataset
      */
-    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
 
     /**
@@ -102,35 +104,6 @@ public class EarthquakeActivity extends AppCompatActivity implements androidx.lo
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(mAdapter);
 
-//        // Get a reference to the ConnectivityManager to check state of network connectivity
-//        ConnectivityManager connMgr = (ConnectivityManager)
-//                getSystemService(Context.CONNECTIVITY_SERVICE);
-//
-//        // Get details on the currently active default data network
-//        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-//
-//        // If there is a network connection, fetch data
-//        if (networkInfo != null && networkInfo.isConnected()) {
-//            // Get a reference to the LoaderManager, in order to interact with loaders.
-//            LoaderManager loaderManager = getLoaderManager();
-//
-//            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-//            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-//            // because this activity implements the LoaderCallbacks interface).
-//            //loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-//            Log.i(LOG_TAG, "TEST: calling initloader()...");
-//            androidx.loader.app.LoaderManager.getInstance(this).initLoader(EARTHQUAKE_LOADER_ID, null, this);
-//
-//        } else {
-//            // Otherwise, display error
-//            // First, hide loading indicator so error message will be visible
-//            View loadingIndicator = findViewById(R.id.loading_indicator);
-//            loadingIndicator.setVisibility(View.GONE);
-//
-//            // Update empty state with no connection error message
-//            mEmptyStateTextView.setText(R.string.no_internet_connection);
-//        }
-
         //Checks Internet Connection and Starts the Loader
         startLoader();
 
@@ -178,7 +151,21 @@ public class EarthquakeActivity extends AppCompatActivity implements androidx.lo
     public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
         // Create a new loader for the given URL
         Log.i(LOG_TAG, "TEST: onCreateLoader() called...");
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", "time");
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
+
     }
 
     @Override
